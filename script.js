bc6814071b915cd451e4cb892f235ba70e4f7a71
AnemoneTK <Copp.teenkung@gmail.com>
2025-03-01 14:45:13 +0700

    create login function

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello");
+// const username = admin;
+// const password = admin1234;
+
+const checkUser = (username, password) => {
+  if (username === "" || password === "") {
+    alert("กรุณากรอกข้อมูลให้ครบถ้วน");
+  } else if (username != "admin" || password != "admin1234") {
+    alert("ข้อมูลไม่ถูกต้อง");
+  } else {
+    alert("เข้าสู่ระบบ!!");
+  }
+};
+const login = () => {
+  const username_input = document.getElementById("username").value;
+  const password_input = document.getElementById("password").value;
+  console.log("Username " + username_input);
+  console.log("Password " + password_input);
+
+  checkUser(username_input, password_input);
+};
